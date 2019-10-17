aff0f45d48e4a7b51d47362a470cc46e1a792c9e
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-17 15:18:09 -0400

    heroku

---
+++ client/build/precache-manifest.1518da76b70df35792f45686d8554bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be6a3345f50eb9abe4b7d524b441836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c736278d9fbb6011f1a",
+    "url": "/static/css/main.bf847cc8.chunk.css"
+  },
+  {
+    "revision": "a43b8c34f475de981885",
+    "url": "/static/js/2.3830dfbf.chunk.js"
+  },
+  {
+    "revision": "9c736278d9fbb6011f1a",
+    "url": "/static/js/main.a8050ab6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c57b7f31a0ff989b42c19f48fcae5db4",
+    "url": "/static/media/original.c57b7f31.gif"
+  }
+]);
