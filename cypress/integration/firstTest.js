167bc4b988290b390a211a57471913600cf24d5d
Shawn <shawn.harris3510@gmail.com>
2020-10-10 17:03:28 -0400

    merged project3 branch with master

--- cypress/integration/firstTest.js
+++ cypress/integration/firstTest.js
@@ -14,6 +14,7 @@ describe("Testing Data Storage", () => {
         cy.get('[placeholder="New Activity"]')
         // type "Read a Book" into input
         .type("Read a Book")
-        
+        // travel up DOM to parent
+        .parent()
     })
 })
