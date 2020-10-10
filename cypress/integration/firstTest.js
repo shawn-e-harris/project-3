f3a85e1d4a4c1d949e2f8679a2219683c7ca8bb0
Shawn <shawn.harris3510@gmail.com>
2020-10-10 16:56:24 -0400

    adding comments before continuing

--- cypress/integration/firstTest.js
+++ cypress/integration/firstTest.js
@@ -1,15 +1,19 @@
-// const { cyan } = require("@material-ui/core/colors")
-// const { IsoTwoTone } = require("@material-ui/icons")
-
 /// <reference types="cypress" />
 
 describe("Testing Data Storage", () => {
 
     it.only("Add activity", () => {
+        // visit baseUrl
         cy.visit("/")
+
+        // locate by attribute with content that contains "Activities"
         cy.contains('[href]', "Activities")
+        // click activities link
         .click()
+        // locate input by attribute name & value
         cy.get('[placeholder="New Activity"]')
+        // type "Read a Book" into input
         .type("Read a Book")
+        
     })
 })
