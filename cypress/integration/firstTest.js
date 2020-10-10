0124f8674510c066a4138717b69d0615acfc1bcd
Shawn <shawn.harris3510@gmail.com>
2020-10-10 19:34:59 -0400

    located Journal href

--- cypress/integration/firstTest.js
+++ cypress/integration/firstTest.js
@@ -2,9 +2,9 @@
 
 describe("Testing Data Storage", () => {
 
-    it.only("Add activity", () => {
+    it("Add Activity", () => {
         // visit baseUrl
-        cy.visit("/")
+        cy.visit('/')
 
         // locate by attribute with content that contains "Activities"
         cy.contains('[href]', "Activities")
@@ -22,8 +22,27 @@ describe("Testing Data Storage", () => {
         .click()
         // travel up DOM to parent
         .parent()
-        // locate child by element
+        // locate child submit by attribute name & value
         .find('[type="submit"]')
+        // click Submit
         .click()
+    })
+
+    it.only('Add Jornal Entry', () => {
+        // visit baseUrl
+        cy.visit('/')
+
+        // locate by attribute with content that contains "Activities"
+        cy.contains('[href]', "Journal")
+        // click activities link
+        .click()
+        // locate input by attribute name & value
+        // type "Read a Book" into input
+        // travel up DOM to parent
+        // locate child radio by attribute name & value 
+        // click Low Activity radio
+        // travel up DOM to parent
+        // locate child submit by attribute name & value
+        // click Submit
     })
 })
