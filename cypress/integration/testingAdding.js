667bba930442feace915c40ea686f348adf60666
Shawn <shawn.harris3510@gmail.com>
2020-10-10 22:15:13 -0400

    testing refactor

---
+++ cypress/integration/testingAdding.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+describe("Testing Adding Activities & Journals", () => {
+
+    beforeEach('Adding Something New', () => {
+        // visit baseUrl
+        cy.visit('/')
+    })
+    it("Add Activity", () => {
+        
+        // locate by attribute with content that contains "Activities"
+        cy.contains('[href]', "Activities")
+        // click activities link
+        .click()
+        // locate input by attribute name & value
+        cy.get('[placeholder="New Activity"]')
+        // type "Read a Book" into input
+        .type("Read a Book")
+        // travel up DOM to parent
+        .parent()
+        // locate child radio by attribute name & value 
+        .find('[value="Low"]')
+        // click Low Activity radio
+        .click()
+        // travel up DOM to parent
+        .parent()
+        // locate child submit by attribute name & value
+        .find('[type="submit"]')
+        // click Submit
+        .click()
+    })
+
+    it('Add Journal Entry', () => {
+        
+        // locate by attribute with content that contains "Activities"
+        cy.contains('[href]', "Journal")
+        // click activities link
+        .click()
+        // locate input by attribute name & value
+        cy.get('[placeholder="New Journal"]')
+        // type "Today I worked on a Cypress test and I feel accomplished!" into input
+        .type('Today I worked on a Cypress test and I feel accomplished!')
+        // travel up DOM to parent w/ form element
+        .parent('form')
+        // click Submit
+        .submit()
+    })
+})
---
+++ cypress/integration/testingRoutes.js
@@ -0,0 +1,8 @@
+describe('Testing Routes', () => {
+
+    it.only('Test All Routes', () => {
+        cy.visit('/')
+        cy.visit('/activities')
+        cy.visit('/journals')
+    })
+})
